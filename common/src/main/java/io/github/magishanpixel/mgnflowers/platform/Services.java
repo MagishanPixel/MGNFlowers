4e2576c72858091362dc480ce898baf912b5255c
MagishanPixel <unknownwhitney500@gmail.com>
2026-08-07 17:18:43 +0700

    fix dependency

--- common/src/main/java/io/github/magishanpixel/mgnflowers/platform/Services.java
+++ common/src/main/java/io/github/magishanpixel/mgnflowers/platform/Services.java
@@ -12,7 +12,7 @@ public class Services {
         final T loadedService = ServiceLoader.load(clazz)
                 .findFirst()
                 .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
-        MGNConstants.LOG.debug("Loaded {} for service {}", loadedService, clazz);
+        //MGNConstants.LOG.debug("Loaded {} for service {}", loadedService, clazz);
         return loadedService;
     }
 }
